8a54b92960a6e8182312d17a89514bf2d69f3825
姜祎楠 <jynwodemeng91901@gmail.com>
2018-06-20 19:21:44 +0200

    add storybook and semantic ui

---
+++ 01-storybook/storybook/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Test of storybook
+      </div>
+    );
+  }
+}
+
+export default App;
