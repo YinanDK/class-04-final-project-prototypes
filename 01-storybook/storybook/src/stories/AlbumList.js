9359fe77ac9ab02c294ae046cd1b3cdae00d4406
姜祎楠 <jynwodemeng91901@gmail.com>
2018-06-23 18:02:33 +0200

    add list

--- 01-storybook/storybook/src/stories/AlbumList.js
+++ 01-storybook/storybook/src/stories/AlbumList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'semantic-ui-css/semantic.min.css';
 import { storiesOf } from '@storybook/react';
 
 import {Grid, Card, Image} from 'semantic-ui-react';
--- 01-storybook/storybook/src/stories/MusicFilter.js
+++ 01-storybook/storybook/src/stories/MusicFilter.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { Select } from 'semantic-ui-react'
+import { Select, Grid, Card, Image, List, Icon, Button} from 'semantic-ui-react';
+import imageFile1 from './assets/imagefile1.jpg';
 
 const options = [
         { key: 'any', value: 'Any',  text: '- Any -' },
@@ -15,7 +16,421 @@ const options = [
 ];
 const storyWelcome = storiesOf('MusicFilter', module)
 .add('MusicFilter', () => (
-    <Select placeholder='Select your music' options={options} />
+	<div>
+		<Select placeholder='Select your music' options={options} size="big" />
+		<Grid style={{marginTop: "50px"}}>
+			<Grid.Row>
+				<Grid.Column computer={8} >
+				<List size="big" >
+					<List.Item style={{ backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+				</List>
+				</Grid.Column>
+				<Grid.Column computer={8}>
+				<List size="big" >
+					<List.Item style={{ backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+					<List.Item style={{backgroundColor: "#f3f3f3", borderRadius: 5, margin: "5px"}}>
+						<List.Content floated="right" style={{margin: "5px auto"}}>
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+
+							<Icon 
+									circular color='teal'
+									name="play circle"
+									style ={{padding:"5px"}}
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='teal'
+									name="cloud download" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+							<Icon 
+									circular color='red'
+									name="heart" 
+									size="small" 
+									//color="#636363"
+									as = 'i'
+							/>
+						</List.Content>
+						<Image avatar src={imageFile1} />
+						<List.Content>
+							<b>Singer name</b>
+							<List.Description style={{fontSize:"12px"}}> Album name </List.Description>
+						</List.Content>
+					</List.Item>
+				</List>
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	</div>
 ));
 
 
